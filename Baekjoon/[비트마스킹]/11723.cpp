deea0beecc5942776cd6568548cf8b79935456da
kwakrhkr59 <89788679+kwakrhkr59@users.noreply.github.com>
2023-08-19 22:21:14 +0900

    [230808] 실5 집합

--- Baekjoon/[비트마스킹]/11723.cpp
+++ Baekjoon/[비트마스킹]/11723.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 /**[백준 11723] 집합
-/* - vector나 map으로 각 원소의 유무를 판단할 경우 시간초과 발생!
-/*   => 총 원소의 개수는 20. 32비트 크기 int의 각 bit에 원소의 유무를 저장
-/* - value의 i번째 bit가 1이면: 현재 집합에 i 존재 O
-/* - value의 i번째 bit가 1이면: 현재 집합에 i 존재 X
-*/
+ * - vector나 map으로 각 원소의 유무를 판단할 경우 시간초과 발생!
+ *   => 총 원소의 개수는 20. 32비트 크기 int의 각 bit에 원소의 유무를 저장
+ * - value의 i번째 bit가 1이면: 현재 집합에 i 존재 O
+ * - value의 i번째 bit가 1이면: 현재 집합에 i 존재 X
+ */
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
